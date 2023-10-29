6a80c96098d8a4f79697326eb5b4284c76c98634
Pavel.Ivanov2 <Pavel.Ivanov2@endava.com>
2023-10-29 09:17:51 +0200

    Added stripe payment

--- src/components/Client/Product.js
+++ src/components/Client/Product.js
@@ -1,14 +1,19 @@
 import styles from '../Company/Product/Product.module.css';
 import { Link } from 'react-router-dom';
+import StripeCheckout from 'react-stripe-checkout';
 
 const Product = ({product}) => {
+    const onToken = (token) =>{
+        console.log(token);
+    };
+
     return (
         <tr>
                     <th>{product.name}</th>
                     <th>{product.description}</th>
                     <th>{product.price}</th>
                     <th>
-                        <Link to={`/company/${product.id}`} className={styles.tableButton}>PAY$</Link>
+                    <StripeCheckout name="Pay with Stripe" token={onToken} stripeKey="pk_test_51O5qWTBAsyNuGIwOUfywMCvhsMCEnB0JOQh3MSkQQJTPUCymmhPekZaUkJVzRC3BOzGbMm35TtY4k8UKBDMPIjS500D4zBrSGG"/>
                         <Link to={`/clientHomePage/products/SoftUni/EthPayment/${product.name} ${product.price}`} className={styles.tableButton}>PayETH</Link>
                     </th>
         </tr>
